62feaff73f50ced7d2287b4f0ecd80764d9b8efc
Noah Cosamano <njc@g.rit.edu>
2024-09-29 23:12:27 -0400

    fixed rounding

--- story_game.py
+++ story_game.py
@@ -1,28 +1,29 @@
 import random
 from googlesearch import search
+import sys
 
 TIME_PERIOD = "2024"
 
 currencies = {
     "USD",
     "DEM",
-    "Yen",
-    "Euro",
-    "Pound",
-    "Yuan",
-    "Ruble",
-    "Rial",
+    "YEN",
+    "EURO",
+    "POUND",
+    "YUAN",
+    "RUBLE",
+    "RIAL",
 }
 
 currency_multipliers = {
     "USD": 1,
     "DEM": 381.25,
-    "Yen": 142.22,
-    "Euro": 0.9,
-    "Pound": 0.75,
-    "Yuan": 7.01,
-    "Ruble": 94.36,
-    "Rial": 42105
+    "YEN": 142.22,
+    "EURO": 0.9,
+    "POUND": 0.75,
+    "YUAN": 7.01,
+    "RUBLE": 94.36,
+    "RIAL": 42105
 }
 
 def rand_country_value():
@@ -66,26 +67,52 @@ def start_country(beginning_country_value):
         return("Iran")
 
 def currency_convert(convert_from,convert_to,amount_of_currency):
+    convert_from = convert_from.upper()
+    convert_to = convert_to.upper()
     if convert_from not in currency_multipliers or convert_to not in currency_multipliers:
-        return "Invalid currency for simulation"
-    amount_in_usd = amount_of_currency / currency_multipliers[convert_from]
-    converted_amount = int(amount_in_usd * currency_multipliers[convert_to])
-    return converted_amount
+        print("Error: Invalid currency.")
+        sys.exit()
 
+    amount_in_usd = float(amount_of_currency) / currency_multipliers[convert_from]
+    converted_amount = float(amount_in_usd * currency_multipliers[convert_to])
+    rounded_number = round(converted_amount, 2)
+    if convert_to == "YEN":
+        rounded_number = int(round(rounded_number,0))
+    return rounded_number
 
 def main():
     beginning_country_value = rand_country_value()
     beginning_class_value = rand_class_value()
     country_name = start_country(beginning_country_value)
     class_name = start_class(beginning_class_value)
+
     article = "the" if country_name in ["USA", "United Kingdom"] else "" # Fix space in else statement
     article_two = "citizen" if class_name in ["Lower Class","Middle Class","Upper Class"] else "" # Fix space in else statement
+   
     print("The year is",TIME_PERIOD,"in",article,start_country(beginning_country_value),". It is your job as a(n)",start_class(beginning_class_value),
-    article_two,"to lead your country to worldwide rule!")
+        article_two,"to lead your country to worldwide rule!")
+    
     print("Currency options are as follows: ",currencies)
+
     convert_from = input("Choose the currency to convert from: ")
-    amount_of_currency = float(input("Enter the amount of currency: "))
+    convert_from = convert_from.upper()
+    if convert_from not in currencies:
+        print("Error: Not a currency type")
+        sys.exit()
+
+    amount_of_currency = input("Enter the amount of currency: ")
+    try:
+        amount_of_currency = float(amount_of_currency)
+    except ValueError:
+        print("Error: Must be a number")
+        sys.exit()
+
     convert_to = input("Choose the currency to convert to: ")
+    convert_to = convert_to.upper()
+    if convert_to not in currencies:
+        print("Error: Not a currency type")
+        sys.exit()
+
     print(convert_from,"is",currency_convert(convert_from,convert_to,amount_of_currency),"in",convert_to,"currency")
     
 
