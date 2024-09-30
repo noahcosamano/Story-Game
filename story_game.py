6fc4b942a4a0a6cc2891614097686b4cc1850903
Noah Cosamano <njc@g.rit.edu>
2024-09-29 20:55:08 -0400

    working conversion

--- story_game.py
+++ story_game.py
@@ -1,32 +1,28 @@
 import random
+from googlesearch import search
 
 TIME_PERIOD = "2024"
 
-currency_multipliers = {
-    "US_DOL": 1,
-    "GMN_DEM": 381.25,
-    "JPN_YEN": 142.22,
-    "FRN_EUR": 0.9,
-    "UK_PD": 0.75,
-    "CHN_YUAN": 7.01,
-    "RSN_RBL": 94.36,
-    "IRAN_RIAL": 42105
+currencies = {
+    "USD",
+    "DEM",
+    "Yen",
+    "Euro",
+    "Pound",
+    "Yuan",
+    "Ruble",
+    "Rial",
 }
 
-country_to_currency = {
-    "USA": "US_DOL",
-    "United States": "US_DOL",
-    "US": "US_DOL",
-    "Germany": "GMN_DEM",
-    "Japan": "JPN_YEN",
-    "France": "FRN_EUR",
-    "UK": "UK_PD",
-    "United Kingdom": "UK_PD",
-    "Great Britain": "UK_PD",
-    "Britain": "UK_PD",
-    "China": "CHN_YUAN",
-    "Russia": "RSN_RBL",
-    "Iran": "IRAN_RIAL"
+currency_multipliers = {
+    "USD": 1,
+    "DEM": 381.25,
+    "Yen": 142.22,
+    "Euro": 0.9,
+    "Pound": 0.75,
+    "Yuan": 7.01,
+    "Ruble": 94.36,
+    "Rial": 42105
 }
 
 def rand_country_value():
@@ -48,7 +44,7 @@ def start_class(beginning_class_value):
     elif 60 < beginning_class_value <= 90:
         return "Upper Class"
     elif 90 < beginning_class_value <= 100:
-        return "Leading"
+        return "Leader"
     
 def start_country(beginning_country_value):
     """Converts starting value to country"""
@@ -68,29 +64,29 @@ def start_country(beginning_country_value):
         return("Russia")
     elif beginning_country_value == 8:
         return("Iran")
-    
-def currency_conversion(value, currency_type):
-    """Convert the given value using the corresponding currency multiplier."""
-    if currency_type in currency_multipliers:
-        multiplier = currency_multipliers[currency_type]
-        converted_amount = value * multiplier
-        return converted_amount
-    else:
-        raise ValueError("Invalid currency type provided.")
 
-def type_of_currency():
-    country_currency = input("Enter the currency type: ")
-    if currency
-
+def currency_convert(convert_from,convert_to,amount_of_currency):
+    if convert_from not in currency_multipliers or convert_to not in currency_multipliers:
+        return "Invalid currency for simulation"
+    amount_in_usd = amount_of_currency / currency_multipliers[convert_from]
+    converted_amount = int(amount_in_usd * currency_multipliers[convert_to])
+    return converted_amount
 
 
 def main():
     beginning_country_value = rand_country_value()
     beginning_class_value = rand_class_value()
     country_name = start_country(beginning_country_value)
+    class_name = start_class(beginning_class_value)
     article = "the" if country_name in ["USA", "United Kingdom"] else "" # Fix space in else statement
+    article_two = "citizen" if class_name in ["Lower Class","Middle Class","Upper Class"] else "" # Fix space in else statement
     print("The year is",TIME_PERIOD,"in",article,start_country(beginning_country_value),". It is your job as a(n)",start_class(beginning_class_value),
-    "citizen to lead your country to worldwide rule!")
+    article_two,"to lead your country to worldwide rule!")
+    print("Currency options are as follows: ",currencies)
+    convert_from = input("Choose the currency to convert from: ")
+    amount_of_currency = float(input("Enter the amount of currency: "))
+    convert_to = input("Choose the currency to convert to: ")
+    print(convert_from,"is",currency_convert(convert_from,convert_to,amount_of_currency),"in",convert_to,"currency")
     
 
 if __name__ == "__main__":
